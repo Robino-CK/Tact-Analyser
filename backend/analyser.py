1eb1f9a5bfdb209be365968d52e6de698b705684
Robin-CK <robin.kollmann@yahoo.de>
2022-09-19 15:16:23 +0200

    analyser jupyter

--- backend/analyser.py
+++ backend/analyser.py
@@ -1,3 +1,11 @@
 
 #analyse audio data
-#https://www.kdnuggets.com/2020/02/audio-data-analysis-deep-learning-python-part-1.htmly
+#https://www.kdnuggets.com/2020/02/audio-data-analysis-deep-learning-python-part-1.html
+import matplotlib.pyplot as plt
+import librosa.display
+import IPython.display as ipd
+plt.figure(figsize=(14, 5))
+audio_data = 'res/test.wav'
+ipd.Audio(audio_data)
+x , sr = librosa.load(audio_data)
+librosa.display.waveshow(x, sr=sr)
--- backend/takt.py
+++ backend/takt.py
@@ -13,7 +13,7 @@ class Tone:
         self.tone_length = tone_length
         self.frequenz = frequenz
         if (tone_length == 1):
-            self.simple_audio_stamps = self.sa_timestemps[3]
+            self.simple_audio_stamps = self.sa_timestemps[0]
         self.calculate_audio_array()
 
     
@@ -22,7 +22,7 @@ class Tone:
         return self.sa_timestemps.item(index_of_array_ts[0][0] - 1)
 
     def calculate_wave(self):
-        audio_array = np.linspace(0, self.tone_length, self.tone_length * self.sa_timestemps[1], False)
+        audio_array = np.linspace(0, self.tone_length, self.tone_length * self.sa_timestemps[0], False)
         self.wave = (np.sin(self.frequenz * audio_array * 2 * np.pi)  )
     
     def calculate_audio_array(self):
@@ -34,7 +34,7 @@ class Tone:
 
     def play(self):
         # start playback
-        play_obj = sa.play_buffer(self.audio, 1, 2,self.sa_timestemps[1])
+        play_obj = sa.play_buffer(self.audio, 1, 2,self.sa_timestemps[3])
         
         # wait for playback to finish before exiting
         play_obj.wait_done()
@@ -52,7 +52,7 @@ class Song:
         audio = np.hstack((c.wave, d.wave, e.wave, f.wave, g.wave,g.wave, a.wave, a.wave,  g.wave))   
         audio *= 32767 / np.max(np.abs(audio))     
         audio = audio.astype(np.int16)
-        play_obj = sa.play_buffer(audio, 1, 2,11025 * 2)
+        play_obj = sa.play_buffer(audio, 1, 2,11025 * 4)
         play_obj.wait_done()
 
         return

9651239adaad3761f558d8a293d67d2caf2ccb2a
Robin-CK <robin.kollmann@yahoo.de>
2022-10-17 12:11:01 +0200

    Start Stop mit Oberfläche verknüpft

--- backend/takt.py
+++ backend/takt.py
@@ -6,18 +6,17 @@ import threading
 class Takt:
     # docu: https://simpleaudio.readthedocs.io/en/latest/simpleaudio.html
     def __init__(self, bpm):
+        self.stop = False
         self.bpm = bpm
         self.audio_path = 'res/drum_stick.wav'  #sound origin: https://www.fesliyanstudios.com/royalty-free-sound-effects-download/drum-sticks-278
         self.wave_obj = sa.WaveObject.from_wave_file(self.audio_path)
     
-    def play(self, anzahl):
-        for i in range(anzahl):
+    def play(self, stop_event):
+        while not stop_event.is_set():
             play_thread = threading.Thread(target=self.wave_obj.play)
             play_thread.start()
             time.sleep(60.0 / self.bpm)
 
-    def stop(self):
-        return
             
 #takt = Takt(120)
 #takt.play(10)
--- gui/window.py
+++ gui/window.py
@@ -1,4 +1,5 @@
 
+from asyncio.windows_events import NULL
 from PySide6 import QtCore, QtWidgets, QtGui 
 from PySide6.QtWidgets import QApplication, QGridLayout, QPushButton, QStyle, QWidget
 import threading
@@ -20,12 +21,17 @@ class MyWidget(QtWidgets.QWidget):
     def controll_takt(self):
         if (self.recording):
             self.recording = False
-            icon_play = self.style().standardIcon(getattr(QStyle, "SP_MediaPlay"))
-            self.button_takt = QtWidgets.QPushButton(icon_play, "Start Takt")
+            icon_start = self.style().standardIcon(getattr(QStyle, "SP_MediaPlay"))
+            self.button_takt.setText("Start Takt")
+            self.button_takt.setIcon(icon_start)
+            self.stop_event.set()
         else:
             self.recording = True
-            icon_play = self.style().standardIcon(getattr(QStyle, "SP_MediaPause"))
-            self.button_takt = QtWidgets.QPushButton(icon_play, "Stop Takt")
+            icon_stop = self.style().standardIcon(getattr(QStyle, "SP_MediaPause"))
+            self.button_takt.setText("Stop Takt")
+            self.button_takt.setIcon(icon_stop)
             self.takt = Takt(120)
-            self.takt.play(10)
-
+            self.stop_event= threading.Event()
+            self.play_thread = threading.Thread(target=self.takt.play, args= [self.stop_event])
+            self.play_thread.start()
+        
